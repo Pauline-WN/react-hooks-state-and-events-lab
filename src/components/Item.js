7e552fc80c8e1a43e7893d30ccf45b6662000fdd
Pauline-WN <ngurupauline0@gmail.com>
2024-08-09 14:33:04 +0300

    completes lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State variable for cart status
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Toggle cart status on button click
+  function handleCartToggle() {
+    setIsInCart(prevStatus => !prevStatus);
+  }
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCartToggle}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
